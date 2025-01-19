90356617f3c9ddf5200edcb01683829a2ab48f03
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-19 23:27:05 +0300

    Added router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.module.css';
 import Home from './pages/home/home';
+import LoginPage from './pages/login-page/login-page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
