0e23dd242487cf2bd4a468adbd1a125125a9972e
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-19 20:58:45 +0300

    Added dateTime

--- src/components/blog-post/blog-post.jsx
+++ src/components/blog-post/blog-post.jsx
@@ -1,11 +1,12 @@
 import classes from "./blog-post.module.css";
 
-export default function BlogPost({userAvatar, userName, children}) {
+export default function BlogPost({userAvatar, userName, dateTime, children}) {
     return (
         <div className={classes.container}>
             <div className={classes.userInfo}>
                 <img className={classes.avatar} src={userAvatar} alt="avatar" />
                 <p className={classes.name}>{userName}</p>
+                <p className={classes.dateTime}>{dateTime}</p>
             </div>
             <div className={classes.text}>
                 {children}
