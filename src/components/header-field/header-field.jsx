56c41a18511b5190c58f062e5edc1d3bec8a7501
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-20 20:25:31 +0300

    Added loggedIn design

--- src/components/header-field/header-field.jsx
+++ src/components/header-field/header-field.jsx
@@ -3,16 +3,23 @@ import Logo from "../../components/logo/logo";
 import LoginButton from "../../components/login-button/login-button";
 import { useNavigate } from "react-router";
 
-export default function HeaderField({ className }) {
+export default function HeaderField({ isLoggedIn, setIsLoggedIn, className }) {
     let navigate = useNavigate();
 
     return (
         <header className={`${classes.header} ${className}`}>
             <Logo onClick={() => navigate("/")} />
-            <div className={classes.buttons}>
-                <LoginButton onClick={() => navigate("/login")} className={classes.loginButton}>Войти</LoginButton>
-                <LoginButton onClick={() => navigate("/register")} className={classes.regButton}>Создать аккаунт</LoginButton>
-            </div>
+            {isLoggedIn ? (
+                <div className={classes.user}>
+                    <p className={classes.userName}>User Name</p>
+                    <LoginButton onClick={() => setIsLoggedIn(false)}>Выйти</LoginButton>
+                </div>
+            ) : (
+                <div className={classes.buttons}>
+                    <LoginButton onClick={() => navigate("/login")} className={classes.loginButton}>Войти</LoginButton>
+                    <LoginButton onClick={() => navigate("/register")} className={classes.regButton}>Создать аккаунт</LoginButton>
+                </div>
+            )}
         </header>
     );
 }
