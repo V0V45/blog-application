5e2b2a22bc6f23482c7df40b55abced4c005dcf3
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-20 20:25:06 +0300

    Added isLoggedIn state

--- src/index.js
+++ src/index.js
@@ -2,19 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.module.css';
+import { useState } from "react";
 import Home from './pages/home/home';
 import LoginPage from './pages/login-page/login-page';
 import RegPage from './pages/reg-page/reg-page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='login' element={<LoginPage />} />
-        <Route path='register' element={<RegPage />} />
+        <Route path='/' element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path='login' element={<LoginPage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path='register' element={<RegPage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+}
+
+root.render(<App />);
