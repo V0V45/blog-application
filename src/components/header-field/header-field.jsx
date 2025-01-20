5f11e03e0280761794f1e17048d778f02b7223eb
V0V45 <118116637+V0V45@users.noreply.github.com>
2025-01-20 21:58:01 +0300

    Added addPostButton

--- src/components/header-field/header-field.jsx
+++ src/components/header-field/header-field.jsx
@@ -2,6 +2,7 @@ import classes from "./header-field.module.css";
 import Logo from "../../components/logo/logo";
 import LoginButton from "../../components/login-button/login-button";
 import { useNavigate } from "react-router";
+import AddPostButton from "../add-post-button/add-post-button";
 
 export default function HeaderField({ userInfo, setUserInfo, className }) {
     let navigate = useNavigate();
@@ -10,10 +11,13 @@ export default function HeaderField({ userInfo, setUserInfo, className }) {
         <header className={`${classes.header} ${className}`}>
             <Logo onClick={() => navigate("/")} />
             {userInfo ? (
+                <>
+                <AddPostButton className={classes.addPostButton} />
                 <div className={classes.user}>
                     <p className={classes.userName}>{`${userInfo.name} ${userInfo.surname}`}</p>
                     <LoginButton onClick={() => setUserInfo(null)}>Выйти</LoginButton>
                 </div>
+                </>
             ) : (
                 <div className={classes.buttons}>
                     <LoginButton onClick={() => navigate("/login")} className={classes.loginButton}>Войти</LoginButton>
